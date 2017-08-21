b09be525537323e1449fb552a295b2363802bea3
Balyi Mark <balyi.mark@hotmail.com>
2017-08-21 22:52:22 +0200

    Header background animation
    Using extarnal galaxy javascript folder.

--- src/app/widgets/header.widget.ts
+++ src/app/widgets/header.widget.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
+declare function galaxy();
 
 @Component({
   selector: 'app-header',
   template: `
-    <md-toolbar class="header" color="primary">
-      <span>Header</span>
-      <button *ngFor="let btn of buttons" (click)="filter(btn)" md-button>{{btn}}</button>
-    </md-toolbar>
+      <div id="large-header" class="header large-header">
+				<canvas id="galaxy-canvas"></canvas>
+				<h1 class="main-title">
+          {{siteName.split(' ')[0]}} 
+          <span class="thin">{{siteName.split(' ')[1]}}</span>
+        </h1>
+        <h1 class="main-title btns">
+          <span class="thin">
+            <button *ngFor="let btn of buttons" (click)="filter(btn)" md-button>{{btn}}</button>
+          </span>
+        </h1>
+			</div>
   `
 })
 export class HeaderWidget {
     public buttons: String[] = ["Javascript", "Angular", "Java"];
+    public siteName: String = "Interview Questions";
+    ngAfterViewInit() {
+      galaxy();
+    }
 
     public filter(btn: String) {
 
